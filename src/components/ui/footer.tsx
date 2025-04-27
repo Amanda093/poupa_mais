94300e09a63ec0d46f257c0f452404fd8aad48bb
Chrb09 <carloshrbarile@gmail.com>
2025-04-27 16:27:36 -0300

    fix: adicionando background no footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <p className="text-light fixed bottom-0 w-full border-t-[0.15em] border-emerald-500 py-[0.5em] text-center">
+    <div className="text-light fixed bottom-0 w-full border-t-[0.15em] border-emerald-500 bg-white py-[0.5em] text-center">
       As sugestões geradas por IA são apenas estimativas e não substituem a
       orientação de um especialista. Sempre analise com cuidado antes de tomar
       decisões financeiras.
-    </p>
+    </div>
   );
 };
 
